f3690bf43ea5dc0da143b337d273bca9b12e6af9
liumo <liumo@163.com>
2018-04-15 10:12:37 +0800

    start spider

--- novelWeb02/spiders/novelSpider.py
+++ novelWeb02/spiders/novelSpider.py
@@ -1,14 +1,28 @@
 import scrapy
-
+from scrapy import log
 class novelSpider(scrapy.Spider):
     name = "novelSpider"
-    allowed_domains = ["dmoz.org"]
+    # allowed_domains = ["dmoz.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "https://www.zhihu.com/question/20751219"
     ]
 
     def parse(self, response):
+        print("result==="+str(response))
         filename = response.url.split("/")[-2]
+        print("filename==="+str(filename))
         with open(filename, 'wb') as f:
             f.write(response.body)
+
+
+    def parse_details(self, response):
+        item = response.meta.get('item', None)
+        if item:
+            return item
+        else:
+            self.log('No item received for %s' % response.url,
+                     level=log.WARNING)
+
+
+    def closed(reason):
+        print("closeed")
